cf0c07ffb7ee5ae311dfabb621a5c86c3f5b3bfd
Jorge Cossi <jorge_cossi@yahoo.com>
2017-08-11 22:44:47 +0000

    updated show_headings method to list out all possible news article headings

--- news_scraper/lib/news_scraper/cli.rb
+++ news_scraper/lib/news_scraper/cli.rb
@@ -3,15 +3,17 @@ class NewsScraper::CLI
   def call
     puts "Welcome to the News Scraper!"
     puts "Below is a list of news items you can select from."
-    self.show_articles
+    self.show_headings
   end
 
-  def show_articles
+  def show_headings
     puts "DEF SHOW ARTICLES:"
     site = Nokogiri::HTML(open('http://www.nbcnews.com'))
     arr = []
     headings = site.css(".container-gutter .container .panel-group")
-    headings.each{|link| arr << link.css(".panel-group_header .item-heading_group-wrapper .item-heading.item-heading_group").text.strip}
+    headings.each{|link| arr << link.css(".panel-group_header .item-heading.item-heading_group").text.strip}
     arr[0] == arr[0].gsub("Latest","")
+    arr.each.with_index(1){|heading, idx| puts  "#{idx}. #{heading}"}
+    puts headings.css(".panel-group_header .item-heading.item-heading_group").text.strip
   end
 end
