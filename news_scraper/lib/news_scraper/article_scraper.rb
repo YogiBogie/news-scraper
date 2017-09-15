2370d6043b0294270435ba326b43ee43ccf60963
Jorge Cossi <jorge_cossi@yahoo.com>
2017-09-15 18:15:21 +0000

    Working out how to get URL for article user selects

--- news_scraper/lib/news_scraper/article_scraper.rb
+++ news_scraper/lib/news_scraper/article_scraper.rb
@@ -1,5 +1,5 @@
 class NewsScraper::ArticleScraper
-  attr_accessor :title, :noko
+  attr_accessor :title, :noko, :url
   @@all=[]
 
   def initialize(noko=nil)
@@ -8,7 +8,27 @@ class NewsScraper::ArticleScraper
   def url
     code = 0
     @noko.css(".row").each do |link|
-      puts "#{code}. #{link.css("h3").text.strip}"
+      if link.css("h3").text.strip.length > 0 && code < 1#MAYBE I CAN DO SOMETHING WITH THE .eACH OF ROW WHERE I LOOK FOR H3 and then only then store the title and url all at once
+        #link.css('a').each {|l| puts "#1 #{l}"}
+        #puts "LINK= #{link.css('a').first['href']}"
+        ant = 1
+        link.css("h3").each do |h3|
+          puts ant
+          puts h3
+          ant += 1
+        end
+        ent = 1
+        link.css('a').each do |a|
+          if a.css("h3").text.strip.length > 0
+            puts ent
+            puts a
+            ent +=1
+          end
+
+        end#{"LINK= #{h3.css('a')}"
+        #puts "TEXT= #{link.css('h3')}"
+        puts code += 1
+      end
     end
   end
 
